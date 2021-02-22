4a9235e6ea46eb85f346b588af37a5fcc5e2254e
Pavel Zapletal <wardreth@gmail.com>
2021-02-22 14:56:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -63,69 +63,6 @@ function App() {
 
 export default App;
 // function App() {
-// 	const [gameProgress, setGameProgress] = useState([]);
-// 	const [turn, setTurn] = useState(false);
-// 	const [counter, setCounter] = useState(0);
-// 	const inputRef = useRef(null);
-// 	const [isLastStrike, setIsLastStrike] = useState(false);
-
-// 	const result = bowl(gameProgress);
-// 	console.log(result);
-
-// 	const submitHandler = () => {
-// 		let updateState;
-// 		const currentValue = inputRef.current.value;
-
-// 		if (counter === 9 && (currentValue === "X" || currentValue === "x")) {
-// 			setIsLastStrike(!isLastStrike);
-// 			firstTurn();
-// 		} else if (isLastStrike) {
-// 			secondTurn();
-// 		} else if (!turn) firstTurn();
-// 		else if (turn) secondTurn();
-
-// 		console.log(updateState);
-// 		setGameProgress(updateState);
-// 		inputRef.current.value = "";
-
-// 		if (!isLastStrike) setTurn(!turn);
-
-// 		function secondTurn() {
-// 			updateState = [...gameProgress].map((el, index) => {
-// 				if (index === counter - 1) return (el = el + currentValue);
-// 				else return el;
-// 			});
-// 		}
-// 		function firstTurn() {
-// 			if (isLastStrike) console.log("pp");
-// 			updateState = [...gameProgress, currentValue];
-// 			setCounter(prev => prev + 1);
-// 		}
-
-// 		if (currentValue === "x" || currentValue === "X") {
-// 			if (!isLastStrike) setTurn(false);
-// 		}
-// 	};
-// 	return (
-// 		<div>
-// 			<p>Bowling scorer</p>
-// 			<div className="input">
-// 				<input
-// 					ref={inputRef}
-// 					type="text"
-// 					onKeyPress={e => (e.code === "Enter" ? submitHandler() : null)}
-// 				/>
-// 				<button onClick={submitHandler}>submit</button>
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-// export default App;
-
-//-------------------------------
-
-// function App() {
 // 	const compleateGame = [
 // 		"81",
 // 		"9-",
