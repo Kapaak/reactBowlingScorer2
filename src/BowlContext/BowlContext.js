42d927e8528105f9018d74a15d442afffd852a0a
Pavel Zapletal <wardreth@gmail.com>
2021-02-24 09:34:01 +0100

    final version

--- src/BowlContext/BowlContext.js
+++ src/BowlContext/BowlContext.js
@@ -6,14 +6,6 @@ export const BowlContext = createContext();
 export const BowlProvider = ({ children }) => {
 	const [gameProgress, setGameProgress] = useState([]);
 	const result = bowl(gameProgress);
-
-	// try {
-	// 	eval(result); // Missing ' will produce an error
-	// 	result = bowl(gameProgress);
-	// } catch (err) {
-	// 	alert(err);
-	// }
-
 	return (
 		<BowlContext.Provider value={[result, gameProgress, setGameProgress]}>
 			{children}
